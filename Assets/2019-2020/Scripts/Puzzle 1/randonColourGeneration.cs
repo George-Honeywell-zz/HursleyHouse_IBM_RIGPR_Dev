269df818dc00512b49d58128aac45fe788f64c19
LiamMajor97 <57135297+LiamMajor97@users.noreply.github.com>
2019-11-29 14:36:46 +0000

    Simon Says puzzle
    Finished puzzle logic, need to implement into VR and with puzzle model

--- Assets/2019-2020/Scripts/Puzzle 1/randonColourGeneration.cs
+++ Assets/2019-2020/Scripts/Puzzle 1/randonColourGeneration.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class randonColourGeneration : MonoBehaviour
 {
     //Variable Declerations
     int[] sequence;
+    int[] playerSequence;
+    int playerSequencePos = 0;
     int currentDifficulty = 3;
     bool sequencePlayed = true;
     bool onePressed = false;
@@ -13,30 +16,51 @@ public class randonColourGeneration : MonoBehaviour
     bool threePressed = false;
     bool fourPressed = false;
     bool buttonDown = false;
-    public int currentBlock;
-    Renderer objectColour;
+    bool playerMarked = false;
+    bool playerPassed = true;
+    public GameObject redBlock;
+    public GameObject blueBlock;
+    public GameObject yellowBlock;
+    public GameObject greenBlock;
+    public GameObject sequenceBlock;
+    public GameObject SequenceText;
+    public GameObject VictoryText;
+    Renderer redBlockRenderer;
+    Renderer blueBlockRenderer;
+    Renderer yellowBlockRenderer;
+    Renderer greenBlockRenderer;
+    Renderer sequenceBlockRenderer;
+    Text sequenceText;
+    Text victoryText;
 
-
+    //Start function which runs when the script is initialized
     void Start()
     {
-        objectColour = gameObject.GetComponent<Renderer>();
+        //Getting components from the scene
+        redBlockRenderer = redBlock.GetComponent<Renderer>();
+        yellowBlockRenderer = yellowBlock.GetComponent<Renderer>();
+        blueBlockRenderer = blueBlock.GetComponent<Renderer>();
+        greenBlockRenderer = greenBlock.GetComponent<Renderer>();
+        sequenceBlockRenderer = sequenceBlock.GetComponent<Renderer>();
+        sequenceText = SequenceText.GetComponent<Text>();
+        victoryText = VictoryText.GetComponent<Text>();
 
-        if (currentBlock == 5)
+        //Displaying the starting difficulty on screen
+        sequenceText.text = "Current Difficulty: " + currentDifficulty;
+
+        //Creating the sequence arrays
+        sequence = new int[10];
+        playerSequence = new int[10];
+
+        //Assigning the sequence array the values corresponding to the random colour order
+        for (int i = 0; i < 10; i++)
         {
-            //Creating the array
-            sequence = new int[10];
-
-            //Assigning the array the values corresponding to the random colour order
-            for (int i = 0; i < 10; i++)
-            {
-                sequence[i] = Random.Range(0, 4);
-            }
+            sequence[i] = Random.Range(0, 4);
         }
-
-       
     }
 
-   void Update()
+    //Update function which is ran every frame
+    void Update()
     {
         //Plays the current colour sequence to the player when initiated 
         if (sequencePlayed && Input.GetKeyDown("5"))
@@ -45,123 +69,187 @@ public class randonColourGeneration : MonoBehaviour
             sequencePlayed = false;
         }
 
-        if (!buttonDown)
+        //Runs this code when the player inputs the sequence
+        if (playerSequencePos == (currentDifficulty) && !playerMarked)
         {
-            //Code Detecting input from user
-            if (Input.GetKeyDown("1") && currentBlock == 1)
+            //Checks the players sequence against the correct one
+            for (int i = 0; i < currentDifficulty; i++)
             {
-                Debug.Log("1 Is pressed");
-                objectColour.material.SetColor("_Color", Color.white);
-                onePressed = true;
-                buttonDown = true;
-                Debug.Log("Button down is " + buttonDown);
+                if(playerSequence[i] == sequence[i])
+                {
+
+                }
+                else
+                {
+                    playerPassed = false;
+                }
             }
-            else if (Input.GetKeyDown("2") && currentBlock == 2)
+
+            //Stores that the players sequence has been "Marked"
+            playerMarked = true;
+
+            //Displays that the player passed
+            if (playerPassed)
             {
-                Debug.Log("2 Is pressed");
-                objectColour.material.SetColor("_Color", Color.white);
-                twoPressed = true;
-               buttonDown = true;
+                victoryText.text = "Passed, press space to advance";
             }
-            else if (Input.GetKeyDown("3") && currentBlock == 3)
+            //Displays that the player failed
+            else
             {
-                Debug.Log("3 Is pressed");
-                objectColour.material.SetColor("_Color", Color.white);
-                threePressed = true;
-                buttonDown = true;
-            }
-            else if (Input.GetKeyDown("4") && currentBlock == 4)
-            {
-                Debug.Log("4 Is pressed");
-                objectColour.material.SetColor("_Color", Color.white);
-                fourPressed = true;
-                buttonDown = true;
+                victoryText.text = "Failed, press space to retry";
             }
         }
-     
-        if (buttonDown)
+
+        //Runs this code when the player has finished the sequence
+        else if(playerMarked)
         {
-            if (Input.GetKeyUp("1") && currentBlock == 1 && onePressed)
+            if (Input.GetKeyUp("6"))
             {
-                Debug.Log("1 Is released");
-                objectColour.material.SetColor("_Color", Color.red);
-                onePressed = false;
-                buttonDown = false;
+                //If the player passed the previous difficulty, proceeds to the next
+                if (playerPassed)
+                {
+                    playerMarked = false;
+                    currentDifficulty++;
+                    playerSequence = new int[10];
+                    playerSequencePos = 0;
+                    sequenceText.text = "Current Difficulty: " + currentDifficulty;
+                    victoryText.text = "";
+                    playerPassed = true;
+                }
+                //If the player failed the difficulty, repeats it
+                else
+                {
+                    playerMarked = false;
+                    playerSequence = new int[10];
+                    playerSequencePos = 0;
+                    sequenceText.text = "Current Difficulty: " + currentDifficulty;
+                    victoryText.text = "";
+                    playerPassed = true;
+                }
             }
-            else if (Input.GetKeyUp("2") && currentBlock == 2 && twoPressed)
+        }
+        
+        //Runs this code if no other conditions are met
+        else
+        {
+            if (!buttonDown)
             {
-                Debug.Log("2 Is released");
-                objectColour.material.SetColor("_Color", Color.yellow);
-                twoPressed = false;
-                buttonDown = false;
+                //Code Detecting when the user presses down a button
+                if (Input.GetKeyDown("1"))
+                {
+                    Debug.Log("1 Is pressed");
+                    redBlockRenderer.material.SetColor("_Color", Color.white);
+                    onePressed = true;
+                    buttonDown = true;
+                }
+                else if (Input.GetKeyDown("2"))
+                {
+                    Debug.Log("2 Is pressed");
+                    yellowBlockRenderer.material.SetColor("_Color", Color.white);
+                    twoPressed = true;
+                    buttonDown = true;
+                }
+                else if (Input.GetKeyDown("3"))
+                {
+                    Debug.Log("3 Is pressed");
+                    blueBlockRenderer.material.SetColor("_Color", Color.white);
+                    threePressed = true;
+                    buttonDown = true;
+                }
+                else if (Input.GetKeyDown("4"))
+                {
+                    Debug.Log("4 Is pressed");
+                    greenBlockRenderer.material.SetColor("_Color", Color.white);
+                    fourPressed = true;
+                    buttonDown = true;
+                }
             }
-            else if (Input.GetKeyUp("3") && currentBlock == 3 && threePressed)
+
+            if (buttonDown)
             {
-                Debug.Log("3 Is released");
-                objectColour.material.SetColor("_Color", Color.blue);
-                threePressed = false;
-                buttonDown = false;
-            }
-            else if (Input.GetKeyUp("4") && currentBlock == 4 && fourPressed)
-            {
-                Debug.Log("4 Is released");
-                objectColour.material.SetColor("_Color", Color.green);
-                fourPressed = false;
-                buttonDown = false;
+                //Code Dettecting when the user releases a button
+                if (Input.GetKeyUp("1") && onePressed)
+                {
+                    Debug.Log("1 Is released");
+                    redBlockRenderer.material.SetColor("_Color", Color.red);
+                    onePressed = false;
+                    buttonDown = false;
+                    playerSequence[playerSequencePos] = 0;
+                    playerSequencePos++;
+                }
+                else if (Input.GetKeyUp("2") && twoPressed)
+                {
+                    Debug.Log("2 Is released");
+                    yellowBlockRenderer.material.SetColor("_Color", Color.yellow);
+                    twoPressed = false;
+                    buttonDown = false;
+                    playerSequence[playerSequencePos] = 1;
+                    playerSequencePos++;
+                }
+                else if (Input.GetKeyUp("3") && threePressed)
+                {
+                    Debug.Log("3 Is released");
+                    blueBlockRenderer.material.SetColor("_Color", Color.blue);
+                    threePressed = false;
+                    buttonDown = false;
+                    playerSequence[playerSequencePos] = 2;
+                    playerSequencePos++;
+                }
+                else if (Input.GetKeyUp("4") && fourPressed)
+                {
+                    Debug.Log("4 Is released");
+                    greenBlockRenderer.material.SetColor("_Color", Color.green);
+                    fourPressed = false;
+                    buttonDown = false;
+                    playerSequence[playerSequencePos] = 3;
+                    playerSequencePos++;
+                }
             }
         }
     }
 
+    //CoRoutine that displays the current sequence
     IEnumerator ColourSequence()
     {
         //for loop that displays the random colour sequence to the player
         for (int i = 0; i < currentDifficulty; i++)
         {
-            if (currentBlock == 5)
-            {
-                if (sequence[i] == 0)
-                {
-                    objectColour.material.SetColor("_Color", Color.red);
-                    Debug.Log("Number " + (i + 1) + " in sequence is red");
-                    yield return new WaitForSeconds(1);
-                    objectColour.material.SetColor("_Color", Color.white);
-                    yield return new WaitForSeconds(1);
-                }
-                if (sequence[i] == 1)
-                {
-                    objectColour.material.SetColor("_Color", Color.yellow);
-                    Debug.Log("Number " + (i + 1) + " in sequence is yellow");
-                    yield return new WaitForSeconds(1);
-                    objectColour.material.SetColor("_Color", Color.white);
-                    yield return new WaitForSeconds(1);
-                }
-                if (sequence[i] == 2)
-                {
-                    objectColour.material.SetColor("_Color", Color.blue);
-                    Debug.Log("Number " + (i + 1) + " in sequence is blue");
-                    yield return new WaitForSeconds(1);
-                    objectColour.material.SetColor("_Color", Color.white);
-                    yield return new WaitForSeconds(1);
-                }
-                if (sequence[i] == 3)
-                {
-                    objectColour.material.SetColor("_Color", Color.green);
-                    Debug.Log("Number " + (i + 1) + " in sequence is green");
-                    yield return new WaitForSeconds(1);
-                    objectColour.material.SetColor("_Color", Color.white);
-                    yield return new WaitForSeconds(1);
-                }
-            }
 
-            if (currentBlock == 5)
+            if (sequence[i] == 0)
             {
-                if (i == (currentDifficulty - 1))
-                {
-                    objectColour.material.SetColor("_Color", Color.white);
-                }
+                sequenceBlockRenderer.material.SetColor("_Color", Color.red);
+                Debug.Log("Number " + (i + 1) + " in sequence is red");
+                yield return new WaitForSeconds(1);
+                sequenceBlockRenderer.material.SetColor("_Color", Color.white);
+                yield return new WaitForSeconds(1);
+            }
+            if (sequence[i] == 1)
+            {
+                sequenceBlockRenderer.material.SetColor("_Color", Color.yellow);
+                Debug.Log("Number " + (i + 1) + " in sequence is yellow");
+                yield return new WaitForSeconds(1);
+                sequenceBlockRenderer.material.SetColor("_Color", Color.white);
+                yield return new WaitForSeconds(1);
+            }
+            if (sequence[i] == 2)
+            {
+                sequenceBlockRenderer.material.SetColor("_Color", Color.blue);
+                Debug.Log("Number " + (i + 1) + " in sequence is blue");
+                yield return new WaitForSeconds(1);
+                sequenceBlockRenderer.material.SetColor("_Color", Color.white);
+                yield return new WaitForSeconds(1);
+            }
+            if (sequence[i] == 3)
+            {
+                sequenceBlockRenderer.material.SetColor("_Color", Color.green);
+                Debug.Log("Number " + (i + 1) + " in sequence is green");
+                yield return new WaitForSeconds(1);
+                sequenceBlockRenderer.material.SetColor("_Color", Color.white);
+                yield return new WaitForSeconds(1);
             }
         }
 
+        //Sets the suquence boolean to true, allowing the player to replay the boolean
         sequencePlayed = true;
     }
 }
